3a86e1013472b09369025d522ff386826510617f
Dr. Neil Brittliff <Neil.Brittliff@iinet.net.au>
2022-09-02 07:43:22 +1000

    Updated Load

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -15,135 +15,7 @@ const extractFilename = (path) => {
     return pathArray[lastIndex];
 };
 
-$.fn.Load = (filepath) => {
-
-    window.api.fs().readFile(filepath, function(err, data) {
-        var zipFile = new JSZip();
-
-        zipFile.loadAsync(data).then(async function(zipFile) {
-            documents = {};
-            editors[0].setHTML("");
-
-            tree.removeTree();
-
-            tree = createTree('placeholder', 'white', FRAGMENTS_MENU, CALLBACKS);
-
-            var files = zipFile.file(/.*/);
-            var structure = {};
-            var node = null;
-            var rootFolder = null;
-            var setData = false;
-
-            zipFile.forEach(async function(relativePath, zipEntry) {
-
-                if (zipEntry.dir) {
-
-                    if (node == null) {
-                        node = tree.createNode(zipEntry.name.replace(/.$/, ''), false, 'assets/images/folder-icon.png', null, null, 'context1');
-                    } else {
-                        var paths = relativePath.replace(/.$/, '').split("/");
-                        var folder = ""
-
-                        for (var iPath = 0; iPath < paths.length - 1; iPath++) {
-                            folder += paths[iPath] + "/";
-                        }
-
-                        node = structure[folder].createChildNode(paths[paths.length - 1], false, "assets/images/folder-icon.png", null, "context2");
-
-                    }
-
-                    structure[relativePath] = node;
-
-                } else {
-
-                    function getBlob(file) {
-
-                        return new Promise(resolve => {
-                            file.async("blob").then(function(blob) {
-                                resolve(blob);
-                            });
-
-                        });
-
-                    }
-
-                    function readBlob(url) {
-
-                        return new Promise(resolve => {
-                            var reader = new FileReader();
-
-                            reader.onload = function() {
-                                resolve(reader.result);
-                            }
-
-                            reader.readAsText(url);
-
-                        });
-
-                    }
-
-                    let fileUrl = await getBlob(zipEntry);
-                    let data = await readBlob(fileUrl);
-                    let fileName = extractFilename(zipEntry.name);
-                    let path = relativePath.replace(fileName, '');
-
-                    fileName = fileName.split(".html").join("");
-
-                    if (fileName.startsWith('$$')) {
-
-                        if (structure[path] == null) {
-                            node = tree.createNode(path, false, 'assets/images/folder-icon.png', null, null, 'context1');
-                            structure[path] = node;
-                        }
-
-                        documents[structure[path].id] = data;
-
-                        tree.selectedNode = structure[path];
-
-                        if (`${path}` == `${fileName.replace('$$', '')}/` && !setData) {
-                            editors[0].setHTML(data);
-                            setData = true;
-                        }
-
-                    } else {
-                        var fragmentNode = structure[path].createChildNode(fileName, false, "assets/images/document-icon.png", null, "context2");
-
-                        documents[fragmentNode.id] = data;
-
-                    }
-
-                }
-
-            });
-
-            tree.drawTree();
-
-        })
-
-    })
-
-}
-
-$.fn.Join = (childNodes, html, level) => {
-
-    for (let childNode in childNodes) {
-        html.push(documents[childNodes[childNode].id] == null ? '' : documents[childNodes[childNode].id]);
-
-        if (level < 2 && documents[childNodes[childNode].id] != null &&
-            documents[childNodes[childNode].id].trim().length > 0) {
-            html.push(`<div class="break"></div>`);
-        }
-
-        if (childNodes[childNode].childNodes.length > 0) {
-            $(this).Join(childNodes[childNode].childNodes, html, level + 1);
-        }
-
-    }
-
-}
-
 $(async() => {
-
 
     document.getElementById('load_file').addEventListener('click', event => {
         function load(callback) {
@@ -164,7 +36,114 @@ $(async() => {
 
         }
 
-        load(function(files) {
+        load(files => {
+
+            function process(data) {
+                var zipFile = new JSZip();
+
+                zipFile.loadAsync(data).then(async function(zipFile) {
+                    documents = {};
+
+                    document.getElementById('details').innerHTML = "";
+
+                    tree.removeTree();
+
+                    tree = createTree('placeholder', 'white', FRAGMENTS_MENU, CALLBACKS);
+
+                    var files = zipFile.file(/.*/);
+                    var structure = {};
+                    var node = null;
+                    var rootFolder = null;
+                    var setData = false;
+
+                    zipFile.forEach(async function(relativePath, zipEntry) {
+
+                        if (zipEntry.dir) {
+
+                            if (node == null) {
+                                node = tree.createNode(zipEntry.name.replace(/.$/, ''), false, 'assets/images/folder-icon.png', null, null, 'context1');
+                            } else {
+                                var paths = relativePath.replace(/.$/, '').split("/");
+                                var folder = ""
+
+                                for (var iPath = 0; iPath < paths.length - 1; iPath++) {
+                                    folder += paths[iPath] + "/";
+                                }
+
+                                node = structure[folder].createChildNode(paths[paths.length - 1], false, "assets/images/folder-icon.png", null, "context2");
+
+                            }
+
+                            structure[relativePath] = node;
+
+                        } else {
+
+                            function getBlob(file) {
+
+                                return new Promise(resolve => {
+                                    file.async("blob").then(function(blob) {
+                                        resolve(blob);
+                                    });
+
+                                });
+
+                            }
+
+                            function readBlob(url) {
+
+                                return new Promise(resolve => {
+                                    var reader = new FileReader();
+
+                                    reader.onload = function() {
+                                        resolve(reader.result);
+                                    }
+
+                                    reader.readAsText(url);
+
+                                });
+
+                            }
+
+                            let fileUrl = await getBlob(zipEntry);
+                            let data = await readBlob(fileUrl);
+                            let fileName = extractFilename(zipEntry.name);
+                            let path = relativePath.replace(fileName, '');
+
+                            fileName = fileName.split(".html").join("");
+
+                            if (fileName.startsWith('$$')) {
+
+                                if (structure[path] == null) {
+                                    node = tree.createNode(path, false, 'assets/images/folder-icon.png', null, null, 'context1');
+                                    structure[path] = node;
+                                }
+
+                                documents[structure[path].id] = data;
+
+                                tree.selectedNode = structure[path];
+
+                                if (`${path}` == `${fileName.replace('$$', '')}/` && !setData) {
+                                    document.getElementById('details').innerHTML = "";
+                                    setData = true;
+                                }
+
+                            } else {
+                                var fragmentNode = structure[path].createChildNode(fileName, false, "assets/images/document-icon.png", null, "context2");
+
+                                documents[fragmentNode.id] = data;
+
+                            }
+
+                        }
+
+                    });
+
+                    tree.drawTree();
+
+                })
+
+
+            }
 
             function readFile(file) {
 
@@ -176,7 +155,7 @@ $(async() => {
                     reader.onload = function(e) {
                         pdfFile = file;
 
-                        convert(reader.result);
+                        process(reader.result);
 
                     };
 
@@ -187,8 +166,12 @@ $(async() => {
                 }
 
             }
-        });
 
+            if (files.length == 1) {
+                readFile(files[0])
+            }
+
+        });
 
     });
 
@@ -199,9 +182,9 @@ $(async() => {
 
         onclick: function(node) {
             if (documents[node.id] == null) {
-                editors[0].setHTML("");
+                document.getElementById('details').innerHTML = "";
             } else {
-                editors[0].setHTML(documents[node.id]);
+                document.getElementById('details').innerHTML = documents[node.id];
             }
 
         },
